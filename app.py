0f4cc48ef0e42ad68835641dff9eadbf0a2da138
Jay Rathod <42869929+JayRathod341997@users.noreply.github.com>
2025-05-12 15:29:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "requests",
+#     "rich",
+# ]
+# ///
+
+import sys
+import requests
+from rich import print as rprint   
+print(sys.version)
